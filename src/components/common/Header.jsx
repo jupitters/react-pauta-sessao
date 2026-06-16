1091e6c0c653b28a7af5ffd885ca2ae19f6d1807
jupitters <sampaiobale@gmail.com>
2026-06-16 00:02:08 -0300

    fix vite

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -11,25 +11,25 @@ const Header = () => {
                             href="https://www12.senado.leg.br/institucional/responsabilidade-social/acessibilidade/pages/acessibilidade-no-portal-do-senado"
                             role="button" title="Acessibilidade"> <img
                                 src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-acessibilidade.svg"
-                                title="Acessibilidade" width="19px" height="23px" alt="Acessibilidade"> </a> </li>
+                                title="Acessibilidade" width="19px" height="23px" alt="Acessibilidade" /> </a> </li>
                     <li class="fence--solo"></li>
                     <li> <a class="flex-item--center link link-deep js-vlibras" href="#" role="button" title="Libras"> <img
                                 src="https://www.senado.leg.br/noticias/essencial/images/hands.svg" title="Libras" width="21px"
-                                height="23px" alt="Libras"> </a> </li>
+                                height="23px" alt="Libras" /> </a> </li>
                     <li class="fence--solo"></li>
                     <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Alternar contraste"
                             aria-label="Alternar contraste" data-contrast-toggle="" aria-pressed="false"> <img
                                 src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-contraste.svg" width="23px"
-                                height="23px" alt="" aria-hidden="true"> </a> </li>
+                                height="23px" alt="" aria-hidden="true" /> </a> </li>
                     <li class="fence--solo"></li>
                     <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Aumentar zoom"
                             aria-label="Aumentar zoom" data-zoom-in=""> <img
                                 src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-a.svg" width="19px" height="21px"
-                                alt="Aumentar zoom"> </a> </li>
+                                alt="Aumentar zoom" /> </a> </li>
                     <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Diminuir zoom"
                             aria-label="Diminuir zoom" data-zoom-out=""> <img
                                 src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-a-.svg" width="19px" height="21px"
-                                alt="Diminuir zoom"> </a> </li>
+                                alt="Diminuir zoom" /> </a> </li>
                 </ul>
             </nav>
             <nav class="Triad navbar_global u-hideLower">
@@ -43,7 +43,7 @@ const Header = () => {
                 </div>
                 <div class="u-hideLower"> <a class="navbar_global-brand" href="https://www12.senado.leg.br/hpsenado"
                         title="Senado Federal" accesskey="1"> <img
-                            src="https://www.senado.leg.br/noticias/essencial/images/senado.svg" alt="Logo Senado Federal"> </a>
+                            src="https://www.senado.leg.br/noticias/essencial/images/senado.svg" alt="Logo Senado Federal" /> </a>
                 </div>
                 <div class="mr-4">
                     <div class="u-hideLower">
@@ -52,25 +52,25 @@ const Header = () => {
                                     href="https://www12.senado.leg.br/institucional/responsabilidade-social/acessibilidade/pages/acessibilidade-no-portal-do-senado"
                                     role="button" title="Acessibilidade"> <img
                                         src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-acessibilidade.svg"
-                                        title="Acessibilidade" width="19px" height="23px" alt="Acessibilidade"> </a> </li>
+                                        title="Acessibilidade" width="19px" height="23px" alt="Acessibilidade" /> </a> </li>
                             <li class="fence--solo"></li>
                             <li> <a class="flex-item--center link link-deep js-vlibras" href="#" role="button" title="Libras">
                                     <img src="https://www.senado.leg.br/noticias/essencial/images/hands.svg" title="Libras"
-                                        width="21px" height="23px" alt="Libras"> </a> </li>
+                                        width="21px" height="23px" alt="Libras" /> </a> </li>
                             <li class="fence--solo"></li>
                             <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Alternar contraste"
                                     aria-label="Alternar contraste" data-contrast-toggle="" aria-pressed="false"> <img
                                         src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-contraste.svg" width="23px"
-                                        height="23px" alt="" aria-hidden="true"> </a> </li>
+                                        height="23px" alt="" aria-hidden="true" /> </a> </li>
                             <li class="fence--solo"></li>
                             <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Aumentar zoom"
                                     aria-label="Aumentar zoom" data-zoom-in=""> <img
                                         src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-a.svg" width="19px"
-                                        height="21px" alt="Aumentar zoom"> </a> </li>
+                                        height="21px" alt="Aumentar zoom" /> </a> </li>
                             <li> <a class="flex-item--center link link-deep" href="#" role="button" title="Diminuir zoom"
                                     aria-label="Diminuir zoom" data-zoom-out=""> <img
                                         src="https://wwwdsv.senado.leg.br/inc/essencial/img/icon-a-.svg" width="19px"
-                                        height="21px" alt="Diminuir zoom"> </a> </li>
+                                        height="21px" alt="Diminuir zoom" /> </a> </li>
                             <li class="fence--solo"></li>
                             <li> <a class="link link-deep" href="https://www12.senado.leg.br/institucional/falecomosenado"
                                     accesskey="5">Fale com o Senado</a> </li>
