a3f4b5d950898038f88496a9c964a538ec0f3e24
jupitters <sampaiobale@gmail.com>
2026-06-18 20:24:17 -0300

    icons

--- src/components/pauta-sessao/Cabecalho.jsx
+++ src/components/pauta-sessao/Cabecalho.jsx
@@ -1,9 +1,12 @@
 import "./SessaoHeader.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 const SessaoHeader = () => {
     return (
         <div className="text-3xl font-black underline">
             Olar
+            <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCaretRight} /></a>
         </div>
     );
 };
