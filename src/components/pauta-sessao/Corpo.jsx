6bbe98007ed5b2514fbe77648dda8e2fd9667164
jupitters <sampaiobale@gmail.com>
2026-06-22 15:52:43 -0300

    add shadcn

--- src/components/pauta-sessao/Corpo.jsx
+++ src/components/pauta-sessao/Corpo.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const Corpo = () => {
   return (
-    <div>Corpo</div>
+    <Accordion defaultValue={["item-1"]}>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
 
