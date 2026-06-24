cb28f7db96883fe105a6ae25e26ee1a47f3a45b7
jupitters <sampaiobale@gmail.com>
2026-06-24 16:47:01 -0300

    fetch data

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,8 +2,17 @@ import { createContext } from "react";
 
 export const AppContext = createContext();
 
+const fetchDadosEventosLegislativos = async (sessao) => {
+    try{
+        const response = await fetch(`https://www6g.senado.leg.br/dados-eventos-legislativos/resources/sessao/recuperar-sessao?codigoSessao=${sessao}`)
+        const data = response.data;
+    } catch(e) {
+        console.log(e);
+    }
+}
+
 export const AppProvider = ({ children }) => {
-    <AppContext.Provider value={{}} >
+    <AppContext.Provider value={{ fetchDadosEventosLegislativos }} >
         {children}
     </AppContext.Provider>
 };
